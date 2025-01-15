fbb8ec96e1d68c34e07356a53cf42b8f9b73dc68
Jose <joseportillo0823@gmail.com>
2025-01-15 12:15:20 -0800

    create grid

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+boardSize = 16;
 function createBoard(){
     let board = document.querySelector(".board");
-    let test = document.createElement("div");
-    test.classList.add("square");
-    test.textContent = "Hello";
-    board.appendChild(test);
-
+    for(let i = 0; i < boardSize; i++) {
+        let row = document.createElement("div");
+        row.className = "board-row";
+        for(let j = 0; j < boardSize; j++) {
+            let test = document.createElement("div");
+            test.classList.add("cell");
+            row.append(test);
+        }
+        board.append(row);
+    }
 }
 
-createBoard();
+createBoard();  
