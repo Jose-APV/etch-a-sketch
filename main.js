ee096445114bf3743ccd1d4b3582d85329231617
Jose <joseportillo0823@gmail.com>
2025-01-15 13:42:25 -0800

    Create paint effect

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-boardSize = 16;
+let boardSize;
 function createBoard(){
+    let input = prompt("Please enter board size: ");
+    if(input > 100){
+        input = 100;
+    }
+    boardSize = input;
     let board = document.querySelector(".board");
     for(let i = 0; i < boardSize; i++) {
         let row = document.createElement("div");
@@ -7,10 +12,17 @@ function createBoard(){
         for(let j = 0; j < boardSize; j++) {
             let test = document.createElement("div");
             test.classList.add("cell");
+            test.addEventListener("mouseover", ()=>{
+                test.style.backgroundColor = "black";
+            });
             row.append(test);
         }
         board.append(row);
     }
 }
 
-createBoard();  
+function draw() {
+}
+
+createBoard(); 
+draw();
